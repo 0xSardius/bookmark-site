20a67aaa69370b06abd8321498b568c3a70b49d5
Sardius <0xsardius@gmail.com>
2024-08-27 19:54:26 -0400

    complete mobile menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 const tab = document.querySelectorAll(".tab");
 const panel = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+// Hamburger Button Listener
+btn.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-white.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
+}
 
 function onTabClick(e) {
   // Deactivate all tabs
